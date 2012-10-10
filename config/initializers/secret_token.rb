cb276c0da5193b74b6f245a6b434a751b71f23f4
Anthony J Burton <ant@coshx.com>
2012-10-10 00:03:05 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbycMembership::Application.config.secret_token = '0ff7ac4cc13c9de86a65a732f3b5ca59f852272e1b919fdf6d5bbef1c518bc863d6fe1f7cacbdebb3a648a4c8def205e0aaf9f98b075f89dd7f71332f37d3f02'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+FbycMembership::Application.config.session_store :cookie_store, key: '_fbyc_membership_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# FbycMembership::Application.config.session_store :active_record_store
